e37171bb78c2d8dcb067cae2f12e0f29bd0967f0
jpb335 <jpbarnes21@gmail.com>
2024-05-27 08:19:33 -0400

    Update README.md with instructions to run the project. Rename chapter 1 file and refactor

---
+++ 1_basic_histogram/main.py
@@ -0,0 +1,60 @@
+import os
+import collections
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import DataFrameReader, SparkSession
+
+FILE_LOCATION = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "../resources/ml-100k/u.data"
+))
+
+
+def main():
+    _check_resources_exists()
+
+    conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+    sc = SparkContext(conf=conf)
+
+    _run_rdd(spark_context=sc)
+    _run_dataframe(spark_context=sc)
+
+
+def _run_rdd(spark_context: SparkContext):
+    print("### RDD ###")
+
+    lines = spark_context.textFile(FILE_LOCATION)
+
+    ratings = lines.map(lambda line: line.split()[2])
+    result = ratings.countByValue()
+
+    sorted_results = collections.OrderedDict(sorted(result.items(), reverse=True))
+    for key, value in sorted_results.items():
+        print(f"{key} stars: {value}")
+
+    print("### END RDD ###\n\n")
+
+
+def _run_dataframe(spark_context: SparkContext):
+    print("### DATAFRAME ###")
+
+    session = SparkSession(spark_context)
+
+    lines_df = DataFrameReader(spark=session).csv(FILE_LOCATION, sep="\t")
+    ratings_df = (
+        lines_df.groupBy(lines_df.columns[2])
+        .count()
+        .withColumnRenamed(lines_df.columns[2], "Rating")
+    )
+
+    ratings_df.sort("Rating", ascending=False).show()
+
+    print("### END DATAFRAME ###")
+
+
+def _check_resources_exists():
+    if not os.path.exists(FILE_LOCATION):
+        raise FileNotFoundError(f"Expected ml-100k dataset in {FILE_LOCATION}.  See README for download instructions")
+
+
+if __name__ == "__main__":
+    main()
